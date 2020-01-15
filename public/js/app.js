431badb592657e623f142a6f82233c6bb1d98f41
Diego Castillo <djdiego88@gmail.com>
2020-01-14 20:36:06 -0500

    Updated roles and permissions

--- public/js/app.js
+++ public/js/app.js
@@ -121360,8 +121360,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/projects/backend/php/laravel/skolar/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/projects/backend/php/laravel/skolar/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /opt/lampp/htdocs/proyectos/php/laravel/skolar/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/proyectos/php/laravel/skolar/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
